7b723fb11b309d003dc8922a1737ded8d71b9633
Stephan <stephan@takelwerk.net>
2024-09-23 21:32:33 +0200

    Remove podmac

--- packer/templates/takelpodslim/docker/bin/install-podman.bash
+++ packer/templates/takelpodslim/docker/bin/install-podman.bash
@@ -15,6 +15,14 @@ apt-get --yes --no-install-recommends install \
   slirp4netns \
   uidmap
 apt-get clean
+mkdir -p  /var/run/containers/storage
+mkdir -p /var/lib/containers
+cat > /etc/containers/storage.conf <<storage_conf
+[storage]
+driver = "vfs"
+runroot = "/var/run/containers/storage"
+graphroot = "/var/lib/containers"
+storage_conf
 /usr/sbin/useradd \
   --comment 'podman user to run rootless containers' \
   --home-dir /home/podman \
@@ -24,14 +32,3 @@ apt-get clean
   podman
 su podman -c 'mkdir -p /home/podman/.config/containers'
 su podman -c 'echo -e "[storage]\ndriver = \"vfs\"\n" > /home/podman/.config/containers/storage.conf'
-/usr/sbin/useradd \
-  --comment 'podman mac user to run rootless containers' \
-  --gid 20 \
-  --home-dir /home/podmac \
-  --create-home \
-  --shell /bin/bash \
-  --uid 501 \
-  podmac
-usermod --add-subuids 200000-265535 --add-subgids 200000-265535 podmac
-su podmac -c 'mkdir -p /home/podmac/.config/containers'
-su podmac -c 'echo -e "[storage]\ndriver = \"vfs\"\n" > /home/podmac/.config/containers/storage.conf'
